5e824a0cbd6d0d4f646c5902f9b42e6f9f1d013b
mannue <sppe12@gmail.com>
2022-04-17 22:59:08 +0900

    feat($compile): Add judge class
    judge class check user input ballNumbers and return playResult class

---
+++ src/main/java/baseball/Judge.java
@@ -0,0 +1,65 @@
+package baseball;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Judge {
+    private final Map<BallNumber, Integer> ballNumberPlaceMap;
+    private final int SCORE_BUFFER_SIZE = 2;
+    private final int BALL_INDEX = 0;
+    private final int STRIKE_INDEX = 1;
+    private final int NO_KEY_VALUE = -1;
+
+    public Judge() {
+        this.ballNumberPlaceMap = new HashMap<>();
+    }
+
+    public int setup(final BallNumber[] ballNumbers) {
+        if (isInvalidParam(ballNumbers)) {
+            throw new IllegalArgumentException();
+        }
+        if (updatedSize() > 0) {
+            clearTable();
+        }
+        updateTable(ballNumbers);
+        return updatedSize();
+    }
+
+    private boolean isInvalidParam(final BallNumber[] ballNumbers) {
+        return Objects.isNull(ballNumbers) || ballNumbers.length < 1;
+    }
+
+    private void clearTable() {
+        ballNumberPlaceMap.clear();
+    }
+
+    private int updatedSize() {
+        return ballNumberPlaceMap.size();
+    }
+
+    private void updateTable(final BallNumber[] ballNumbers) {
+        for (int i = 1; i <= ballNumbers.length; i++) {
+            ballNumberPlaceMap.put(ballNumbers[i - 1], i);
+        }
+    }
+
+    public PlayResult ask(final BallNumber[] userInputBallNumbers) {
+        if (isInvalidParam(userInputBallNumbers)) {
+            throw new IllegalArgumentException();
+        }
+        final int[] score = new int[SCORE_BUFFER_SIZE];
+        for (int i=1; i <= userInputBallNumbers.length; i++) {
+            writeScore(score, ballNumberPlaceMap.getOrDefault(userInputBallNumbers[i-1],NO_KEY_VALUE), i);
+        }
+        return new PlayResult(userInputBallNumbers.length,score[BALL_INDEX], score[STRIKE_INDEX]);
+    }
+
+    private void writeScore(final int[] score, final int savedBallNumberIndex, int inputBallNumberIndex) {
+        if (Objects.equals(savedBallNumberIndex, NO_KEY_VALUE)) {
+            return ;
+        }
+        int scoreIndex = Objects.equals(savedBallNumberIndex, inputBallNumberIndex) ? STRIKE_INDEX : BALL_INDEX;
+        score[scoreIndex] += 1;
+    }
+}
---
+++ src/test/java/baseball/JudgeTest.java
@@ -0,0 +1,119 @@
+package baseball;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForInterfaceTypes.assertThat;
+
+public class JudgeTest {
+    private final static Judge judge = new Judge();
+    private final int startRange = 1;
+    private final int endRange = 9;
+
+    @DisplayName("Judge setup test")
+    @ParameterizedTest
+    @MethodSource("provideIntegerArrayForBallNumbers")
+    public void testSetup(final int[] numbers) {
+        judgeSetupUsingMockBallGenerator(numbers);
+    }
+
+    private void judgeSetupUsingMockBallGenerator(int[] numbers) {
+        templateUsingBallGeneratorMock(ballGenerator -> {
+            ballGenerator.when(() -> BallGenerator.create(startRange,endRange, numbers.length)).thenReturn(mockBallGeneratorReturnValues(numbers));
+            int updatedCount = judge.setup(BallGenerator.create(startRange,endRange, numbers.length));
+            assertThat(updatedCount).isEqualTo(numbers.length);
+            ballGenerator.verify(() -> BallGenerator.create(startRange,endRange, numbers.length));
+        });
+    }
+
+    private static Stream<Arguments> provideIntegerArrayForBallNumbers() {
+        return Stream.of(
+                Arguments.of((Object) new int[]{1,2,3}),
+                Arguments.of((Object) new int[]{4,5,6}),
+                Arguments.of((Object) new int[]{7,8,9})
+        );
+    }
+
+    @DisplayName("Judge setup parameter is empty")
+    @ParameterizedTest
+    @MethodSource("provideInvalidBallNumbers")
+    public void testSetupParamIsEmpty(final BallNumber[] invalidParam) {
+        assertThatThrownBy(() -> judge.setup(invalidParam))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<Arguments> provideInvalidBallNumbers() {
+        return Stream.of(
+                Arguments.of((Object) null),
+                Arguments.of((Object) new BallNumber[0])
+        );
+    }
+
+    @DisplayName("Judge ask test")
+    @ParameterizedTest
+    @MethodSource("provideMockNumbersAndInputBallNumbersAndExpectedPlayResult")
+    public void testAsk(final int[] mockNumbers, final BallNumber[] userInputBallNumbers, final PlayResult expectedPlayResult) {
+        judgeSetupUsingMockBallGenerator(mockNumbers);
+        PlayResult result = judge.ask(userInputBallNumbers);
+        assertThat(result).isEqualTo(expectedPlayResult);
+    }
+
+    private static Stream<Arguments> provideMockNumbersAndInputBallNumbersAndExpectedPlayResult() {
+        return Stream.of(
+                Arguments.of(
+                        new int[]{1,2,3},
+                        makeBallNumbersByString("1,4,5"),
+                        new PlayResult(3,0,1)),
+
+                Arguments.of(
+                        new int[]{2,4,5},
+                        makeBallNumbersByString("4,2,6"),
+                        new PlayResult(3,2,0)),
+
+                Arguments.of(
+                        new int[]{4,5,6},
+                        makeBallNumbersByString("4,5,6"),
+                        new PlayResult(3,0,3)
+                )
+        );
+    }
+
+    @DisplayName("Judge ask invalid param test")
+    @ParameterizedTest
+    @MethodSource("provideInvalidBallNumbers")
+    public void testAskInvalidParams(final BallNumber[] invalidParam) {
+        assertThatThrownBy(() -> judge.ask(invalidParam))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static BallNumber[] makeBallNumbersByString(String input) {
+        String[] values = input.split(",");
+        BallNumber[] res = new BallNumber[values.length];
+        for (int i=0; i < values.length; i++) {
+            res[i] = new BallNumber(values[i]);
+        }
+        return res;
+    }
+
+    private void templateUsingBallGeneratorMock(Consumer<MockedStatic<BallGenerator>> consumer) {
+        try(MockedStatic<BallGenerator> ballGeneratorMockedStatic = Mockito.mockStatic(BallGenerator.class)) {
+            consumer.accept(ballGeneratorMockedStatic);
+        }
+    }
+
+    private BallNumber[] mockBallGeneratorReturnValues(int ... values) {
+        BallNumber[] ballNumbers = new BallNumber[values.length];
+        for (int i=1; i <= values.length; i++) {
+            ballNumbers[i-1] = new BallNumber(values[i-1]);
+        }
+        return ballNumbers;
+    }
+}
